cba2bf54c9b918b371fe00754b90c42816eb63fd
khushvant kumar <98275946+khushvantkr12@users.noreply.github.com>
2025-02-16 01:27:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL:"http://localhost:5000/api",
+	baseURL:"https://megamart-wt6l.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
