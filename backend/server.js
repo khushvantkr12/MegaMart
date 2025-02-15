d9388bc79fbc51bb4a988ec416808ea97912676e
khushvant kumar <98275946+khushvantkr12@users.noreply.github.com>
2025-02-16 01:26:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://mega-mart-iota.vercel.app/"],
     credentials:true
 }));
 
